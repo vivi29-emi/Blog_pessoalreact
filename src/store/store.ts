a4ceb17712fe2965d42811ea01e47e7bf62d077b
Viviane Emilie <vivi29emi@gmail.com>
2023-04-07 22:31:17 -0300

    Alterado import {legacy_createStore as createStore} from 'redux' classe stare

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { createStore} from "redux";
+import {legacy_createStore as createStore} from 'redux'
 import { tokenReducer } from "./tokens/tokensReducer";
 
 // método que vai interceptar entre o action e o reducer harmazendo o processo.
